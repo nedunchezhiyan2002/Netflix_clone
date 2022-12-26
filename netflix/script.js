cf6fd53f2a3d4710d8d290091e45023d7babdf01
nedunchezhiyan2002 <85939847+nedunchezhiyan2002@users.noreply.github.com>
2022-12-26 19:57:17 +0530

    Add files via upload

---
+++ netflix/script.js
@@ -0,0 +1,111 @@
+var x = document.querySelector(".qsn1");
+      x.addEventListener("click", fun1);
+      var y = document.querySelector(".qsn1 .plus");
+      var z = document.querySelector(".output1");
+
+      function fun1(e) {
+        if (y.textContent == "+") {
+          y.innerHTML = "x";
+          z.className = "output pad";
+          z.innerHTML =
+            "Netflix is a streaming service that offers a wide variety of award-winning TV shows, movies, anime, documentaries and more – on thousands of internet-connected devices.";
+          z.innerHTML +=
+            "You can watch as much as you want, whenever you want, without a single ad – all for one low monthly price. There's always something new to discover, and new TV shows and movies are added every week!";
+        } else {
+          y.innerHTML = "+";
+          z.innerHTML = " ";
+          z.className = "output";
+        }
+      }
+
+      var x2 = document.querySelector(".qsn2");
+      x2.addEventListener("click", fun2);
+      var y2 = document.querySelector(".qsn2 .plus");
+      var z2 = document.querySelector(".output2");
+
+      function fun2(e) {
+        if (y2.textContent == "+") {
+          y2.innerHTML = "x";
+          z2.className = "output pad";
+          z2.innerHTML =
+            "Watch Netflix on your smartphone, tablet, Smart TV, laptop, or streaming device, all for one fixed monthly fee. Plans range from ₹ 199 to ₹ 799 a month. No extra costs, no contracts.";
+        } else {
+          y2.innerHTML = "+";
+          z2.innerHTML = " ";
+          z2.className = "output";
+        }
+      }
+      var x3 = document.querySelector(".qsn3");
+      x3.addEventListener("click", fun3);
+      var y3 = document.querySelector(".qsn3 .plus");
+      var z3 = document.querySelector(".output3");
+
+      function fun3(e) {
+        if (y3.textContent == "+") {
+          y3.innerHTML = "x";
+          z3.className = "output pad";
+          z3.innerHTML =
+            "Watch anywhere, anytime, on an unlimited number of devices. Sign in with your Netflix account to watch instantly on the web at netflix.com from your personal computer or on any internet-connected device that offers the Netflix app, including smart TVs, smartphones, tablets, streaming media players and game consoles.";
+          z3.innerHTML +=
+            "You can also download your favourite shows with the iOS, Android, or Windows 10 app. Use downloads to watch while you're on the go and without an internet connection. Take Netflix with you anywhere.";
+        } else {
+          y3.innerHTML = "+";
+          z3.innerHTML = " ";
+          z3.className = "output";
+        }
+      }
+      var x4 = document.querySelector(".qsn4");
+      x4.addEventListener("click", fun4);
+      var y4 = document.querySelector(".qsn4 .plus");
+      var z4 = document.querySelector(".output4");
+
+      function fun4(e) {
+        if (y4.textContent == "+") {
+          y4.innerHTML = "x";
+          z4.className = "output pad";
+          z4.innerHTML =
+            "Netflix is flexible. There are no annoying contracts and no commitments. You can easily cancel your account online in two clicks. There are no cancellation fees – start or stop your account anytime.";
+        } else {
+          y4.innerHTML = "+";
+          z4.innerHTML = " ";
+          z4.className = "output";
+        }
+      }
+
+      var x5 = document.querySelector(".qsn5");
+      x5.addEventListener("click", fun5);
+      var y5 = document.querySelector(".qsn5 .plus");
+      var z5 = document.querySelector(".output5");
+
+      function fun5(e) {
+        if (y5.textContent == "+") {
+          y5.innerHTML = "x";
+          z5.className = "output pad";
+          z5.innerHTML =
+            "Netflix has an extensive library of feature films, documentaries, TV shows, anime, award-winning Netflix originals, and more. Watch as much as you want, anytime you want.";
+        } else {
+          y5.innerHTML = "+";
+          z5.innerHTML = " ";
+          z5.className = "output";
+        }
+      }
+
+      var x6 = document.querySelector(".qsn6");
+      x6.addEventListener("click", fun6);
+      var y6 = document.querySelector(".qsn6 .plus");
+      var z6 = document.querySelector(".output6");
+
+      function fun6(e) {
+        if (y6.textContent == "+") {
+          y6.innerHTML = "x";
+          z6.className = "output pad";
+          z6.innerHTML =
+            "Watch anywhere, anytime, on an unlimited number of devices. Sign in with your Netflix account to watch instantly on the web at netflix.com from your personal computer or on any internet-connected device that offers the Netflix app, including smart TVs, smartphones, tablets, streaming media players and game consoles.";
+          z6.innerHTML +=
+            "You can also download your favourite shows with the iOS, Android, or Windows 10 app. Use downloads to watch while you're on the go and without an internet connection. Take Netflix with you anywhere.";
+        } else {
+          y6.innerHTML = "+";
+          z6.innerHTML = " ";
+          z6.className = "output";
+        }
+      }
